9e011cb30e2fdf15d3bf1a5bdf1476bfeccf623e
Akshay Dhawle <akshaydhawle531@gmail.com>
2022-01-11 20:11:09 +0530

    first commit

---
+++ resources/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+   
+    $('.js--section-features').waypoints(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    });
+    
+});
